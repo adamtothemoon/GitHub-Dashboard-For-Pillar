3440b9e65192b2f46ef231ab9b6bebe64f87b023
adam to the moon <l0vest0pwn@gmail.com>
2019-07-28 12:38:26 -0400

    feat: working http serverlet and node setup

--- src/Controller/top-repos-by-contributors-controller.js
+++ src/Controller/top-repos-by-contributors-controller.js
@@ -1,3 +1,7 @@
+//[For Review] There is a choice to be made here. We can refactor this code such that the front end hits one unified api
+//and passes a paramiter with the request that tells us which metric to prioritize (starts, contrib etc..).
+//This will made the code harder to read in my opinion but will allow for less duplicated code. Happy to do either. 
+
 export const get = async req => {
     let response;
     try {
